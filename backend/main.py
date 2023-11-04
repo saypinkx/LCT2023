7238d8082078beae68519b6b0402d1cc61248e12
sixzerx <paprut538@gmail.com>
2023-11-04 15:38:04 +0300

    add rout users in main

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 from fastapi import HTTPException
 from pydantic import BaseModel
 
-from routers import students
+from routers import students, users
 
 app = FastAPI()
 
 app.include_router(students.router)
+app.include_router(users.router)
 
 
 @app.get('/proba')
